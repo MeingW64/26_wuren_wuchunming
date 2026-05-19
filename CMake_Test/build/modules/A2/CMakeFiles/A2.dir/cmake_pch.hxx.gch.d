2184afcd3a9c66baa8f127bb96f0ccb2d735d416
MeingW64 <1923335896@qq.com>
2026-05-19 22:21:35 +0800

    CMake作业

---
+++ CMake_Test/build/modules/A2/CMakeFiles/A2.dir/cmake_pch.hxx.gch.d
@@ -0,0 +1,17 @@
+modules/A2/CMakeFiles/A2.dir/cmake_pch.hxx.gch: \
+ /home/wu/桌面/2026_wuren_wuchunming/CMake_Test/build/modules/A2/CMakeFiles/A2.dir/cmake_pch.hxx.cxx \
+ /usr/include/stdc-predef.h \
+ /home/wu/桌面/2026_wuren_wuchunming/CMake_Test/build/modules/A2/CMakeFiles/A2.dir/cmake_pch.hxx \
+ /usr/include/c++/11/cstddef \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/cpu_defines.h \
+ /usr/include/c++/11/pstl/pstl_config.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h
---
+++ CMake_Test/build/modules/A2/CMakeFiles/A2.dir/src/A2.cpp.o.d
@@ -0,0 +1,33 @@
+modules/A2/CMakeFiles/A2.dir/src/A2.cpp.o: \
+ /home/wu/桌面/2026_wuren_wuchunming/CMake_Test/modules/A2/src/A2.cpp \
+ /usr/include/stdc-predef.h \
+ /home/wu/桌面/2026_wuren_wuchunming/CMake_Test/modules/A2/include/A2.h \
+ /usr/include/c++/11/deque /usr/include/c++/11/bits/stl_algobase.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
+ /usr/include/c++/11/bits/functexcept.h \
+ /usr/include/c++/11/bits/exception_defines.h \
+ /usr/include/c++/11/bits/cpp_type_traits.h \
+ /usr/include/c++/11/ext/type_traits.h \
+ /usr/include/c++/11/ext/numeric_traits.h \
+ /usr/include/c++/11/bits/stl_pair.h /usr/include/c++/11/bits/move.h \
+ /usr/include/c++/11/type_traits \
+ /usr/include/c++/11/bits/stl_iterator_base_types.h \
+ /usr/include/c++/11/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/11/bits/concept_check.h \
+ /usr/include/c++/11/debug/assertions.h \
+ /usr/include/c++/11/bits/stl_iterator.h \
+ /usr/include/c++/11/bits/ptr_traits.h /usr/include/c++/11/debug/debug.h \
+ /usr/include/c++/11/bits/predefined_ops.h \
+ /usr/include/c++/11/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++allocator.h \
+ /usr/include/c++/11/ext/new_allocator.h /usr/include/c++/11/new \
+ /usr/include/c++/11/bits/exception.h \
+ /usr/include/c++/11/bits/memoryfwd.h \
+ /usr/include/c++/11/bits/stl_construct.h \
+ /usr/include/c++/11/bits/stl_uninitialized.h \
+ /usr/include/c++/11/ext/alloc_traits.h \
+ /usr/include/c++/11/bits/alloc_traits.h \
+ /usr/include/c++/11/bits/stl_deque.h \
+ /usr/include/c++/11/initializer_list \
+ /usr/include/c++/11/bits/range_access.h \
+ /usr/include/c++/11/bits/deque.tcc
